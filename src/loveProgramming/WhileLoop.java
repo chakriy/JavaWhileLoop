c8a154e37588f0ea4155e966a4d11a53ee404d35
chakriy <ychakri@yahoo.com>
2019-01-05 14:49:46 +0000

    Java WhileLoop

---
+++ src/loveProgramming/WhileLoop.java
@@ -0,0 +1,11 @@
+package loveProgramming;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int a =1;
+        while(a<=100) {
+            System.out.println("The Value of a is "+a);
+            a++;
+        }
+    }
+}
